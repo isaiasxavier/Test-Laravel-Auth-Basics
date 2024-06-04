896c6678456857e562b611196e7e1264f235273c
Isaias Xavier Santana <isaiasxavier@outlook.com>
2024-06-04 20:33:58 +0200

    Solution by Isaias Xavier

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -29,8 +29,9 @@
                                      class="block mt-1 w-full"
                                      type="text"
                                      name="name"
-                                     value="???"
+                                     value="{{ Auth::user()->name }}"
                                      required />
+
                         </div>
 
                         <div class="mt-4">
@@ -40,7 +41,7 @@
                                      class="block mt-1 w-full"
                                      type="email"
                                      name="email"
-                                     value="???"
+                                     value="{{ Auth::user()->email }}"
                                      required />
                         </div>
 
